0d63c6f5228d6caf03096bbe5af15013844b0259
Oksana Dziuba <dziuba131303@gmail.com>
2023-04-17 03:25:46 +0400

    edits 8th task

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,5 +1,25 @@
-refs = {
-  form: document.querySelector(".login-form"),
+const refs = {
+  formSubmitBtn: document.querySelector(".login-form"),
 };
 
-form.addEventListener("submit");
+refs.formSubmitBtn.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const formElements = event.currentTarget.elements;
+
+  const mail = formElements.email.value;
+  const password = formElements.password.value;
+
+  console.log(mail, password);
+  const formData = {
+    mail,
+    password,
+  };
+
+  if (mail === "" || password === "") {
+    return alert("Всі поля повинні бути заповнені!");
+  }
+  console.log(formData);
+  console.log(formData.reset);
+}
