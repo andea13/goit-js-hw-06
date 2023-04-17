8490c319f8e98853688643bc61d10756202eac6b
Oksana Dziuba <dziuba131303@gmail.com>
2023-04-17 16:38:36 +0400

    adds solution for the 9th task

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const onChangeColorButtonClick = document.querySelector(".change-color");
+const onColorValueDisplay = document.querySelector(".color");
+const onBodyBackgroundColorDisplay = document.querySelector("body");
+
+onChangeColorButtonClick.addEventListener("click", changeBackgroundColor);
+
+function changeBackgroundColor(event) {
+  onColorValueDisplay.textContent = getRandomHexColor();
+  onBodyBackgroundColorDisplay.style.backgroundColor =
+    onColorValueDisplay.textContent;
+}
--- src/js/task-10.js
+++ src/js/task-10.js
@@ -3,3 +3,28 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  controls: document.querySelector("#controls"),
+  createBtn: document.querySelector("[data-create]"),
+  destroyBtn: document.querySelector("[data-destroy]"),
+  boxesContainer: document.querySelector("#boxes"),
+};
+
+refs.createBtn.addEventListener("click", createBoxes);
+// refs.destroyBtn.addEventListener("click", destroyBoxes);
+
+function createBoxes(amount) {
+  let outputValue = refs.controls.children[0].value;
+  amount = +outputValue;
+  let oneDiv = document.createElement("div");
+  console.log(oneDiv);
+  let markup = `${oneDiv} * ${amount}`;
+  const addedDivs = refs.boxesContainer.insertAdjacentHTML(
+    "afterbegin",
+    markup
+  );
+  // let numberOfDivsToCreate = addedDiv * outputValue;
+  // console.log(numberOfDivsToCreate.append);
+  // console.log(addedDiv);
+}
